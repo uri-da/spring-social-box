0edab98cc6310c4fbc7743dfc71197c916eb305c
mpoitras <mathieu_poitras@silanis.com>
2014-11-28 14:06:36 -0500

    Moved to slf4j-api for logging to give flexibility to client code. Removed the SEVERE log and replaced with info. The client code should be the one handling the errors.

--- src/main/java/org/springframework/social/box/rest/errorhandling/BoxRestTemplateErrorHandler.java
+++ src/main/java/org/springframework/social/box/rest/errorhandling/BoxRestTemplateErrorHandler.java
@@ -1,13 +1,13 @@
 package org.springframework.social.box.rest.errorhandling;
 
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.ResponseErrorHandler;
 
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * User: greg
@@ -16,12 +16,12 @@ import java.util.logging.Logger;
  */
 public class BoxRestTemplateErrorHandler extends DefaultResponseErrorHandler implements ResponseErrorHandler {
 
-    private static final Logger log = Logger.getLogger(BoxRestTemplateErrorHandler.class.getName());
+    private static final Logger log = LoggerFactory.getLogger(BoxRestTemplateErrorHandler.class.getName());
 
     @Override
     public boolean hasError(ClientHttpResponse response) throws IOException {
         if(super.hasError(response)){
-            log.log(Level.SEVERE, IOUtils.toString(response.getBody()));
+            log.info(IOUtils.toString(response.getBody()));
             return true;
         } else {
             return false;
